3e15f9e8ca07a4eeb25da104bef146d4a0f059ed
Charles Faso <fasocharlie@gmail.com>
2026-03-02 19:28:53 -0500

    Scene merge

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -17,56 +17,57 @@ public class InventoryManager : MonoBehaviour
 
     void Start()
     {
-        GenerateIcons();
+        // TODO Install RuntimePreviewGenerator package
+        // GenerateIcons();
     }
 
     void GenerateIcons()
     {
-        RuntimePreviewGenerator.BackgroundColor = new Color(0, 0, 0, 0); 
-        RuntimePreviewGenerator.OrthographicMode = true; 
+        // RuntimePreviewGenerator.BackgroundColor = new Color(0, 0, 0, 0); 
+        // RuntimePreviewGenerator.OrthographicMode = true; 
 
         foreach (var item in allItems)
-        {
-            Debug.Log("Generating icon for: " + item.itemName);
-            if (item.holdPrefab != null && item.icon == null)
             {
-                Texture2D preview = RuntimePreviewGenerator.GenerateModelPreview(
-                item.holdPrefab.transform, 128, 128, true
-            );
-                item.icon = Sprite.Create(
-                    preview,
-                    new Rect(0, 0, preview.width, preview.height),
-                    new Vector2(0.5f, 0.5f)
-                );
+            Debug.Log("Generating icon for: " + item.itemName);
+        if (item.holdPrefab != null && item.icon == null)
+
+        i
+                // Texture2D preview = RuntimePreviewGenerator.GenerateModelPreview(
+                //item.holdPrefab.transform, 128, 128, true
+                // );
+                //item.icon = Sprite.Create(
+                //     preview,
+                //     new Rect(0, 0, preview.width, preview.height),
+                //     new Vector2(0.5f, 0.5f)
+                // );
             }
-        }
+}
     }
 
     public void SortByColor()
     {
-        displayedItems = allItems.OrderBy(item =>
-        {
-        Color.RGBToHSV(item.primaryColor, out float h, out float s, out float v);
+{
+    {
+    {
         return h;
-        }).ToList();
+    }).ToList();
+    }).ToList();
 
-        RefreshUI();
-    }
+    RefreshUI();
+}
 
-    public void SearchByName(string query)
+public void SearchByName(string query)
+{
+    if (string.IsNullOrEmpty(query))
+        displayedItems = new List<ItemData>(allItems);
+    else
     {
-        if (string.IsNullOrEmpty(query))
-            displayedItems = new List<ItemData>(allItems);
-        else
-        {
-            displayedItems = allItems.Where(allItems => allItems.itemName.ToLower().Contains(query.ToLower())).ToList();
-        }
-
-        SortByColor();
+        displayedItems = allItems.Where(allItems => allItems.itemName.ToLower().Contains(query.ToLower())).ToList();
     }
 
-    void RefreshUI()
-    {
-        InventoryUI.Instance.Refresh(displayedItems);
-    }
+    SortByColor();
+}
+
+{
+{
 }
